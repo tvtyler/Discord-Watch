38c5ab7e9b46fff2803c40430e4b90d4361a59b6
tvtyler <vance.82@wright.edu>
2024-06-08 19:37:18 -0400

    testing functionality of bot

--- index.js
+++ index.js
@@ -13,4 +13,18 @@ const client = new Client ({
     ]
 });
 
+const TARGET_USERNAME = 'That1Dude'; // The username you want to listen to
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    // Check if the message author is the target user and the message is not from a bot
+    if (message.author.username === TARGET_USERNAME && !message.author.bot) {
+        // Respond by copying the message content
+        message.channel.send(message.content);
+    }
+});
+
 client.login(process.env.DISCORD_TOKEN);

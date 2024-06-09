f610f615a0ec8a3dbb85053192e9604d56188ce9
tvtyler <vance.82@wright.edu>
2024-06-08 23:04:05 -0400

    testing @ sign

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ const client = new Client({
 });
 
 const TARGET_USER_ID = '694018105701171210';
-const CHANNEL_ID = '987760674312704032';
+const CHANNEL_ID = '1239347161242669136';
 let daysMissed = 0; 
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
-    // Schedule a job to run every day at 3 AM UTC (10 PM Eastern Time during standard time)
+    // // Schedule a job to run every day at 5 PM UTC (equivalent to noon Eastern Time during standard time)
+    // schedule.scheduleJob('0 17 * * *', checkUserActivity);
+
+    // debugging
     schedule.scheduleJob('* * * * *', checkUserActivity);
 });
 
@@ -29,24 +33,25 @@ client.on('voiceStateUpdate', (oldState, newState) => {
         if (daysMissed > 0) {
             // Send a message to the channel when daysMissed resets to 0
             const channel = client.channels.cache.get(CHANNEL_ID);
-            channel.send(`${newState.member.user.username} is gaming with the homies. The counter has reset to 0 days.`);
+            channel.send(`<@${newState.member.user.id}> is gaming with the homies. The counter has reset to 0 days.`);
         }
         daysMissed = 0;
     }
 });
 
 function checkUserActivity() {
+
     const guild = client.guilds.cache.get('987760673197027377'); 
     const targetUser = guild.members.cache.get(TARGET_USER_ID);
 
     // Check if the user is in a voice channel
     if (targetUser.voice.channelId) {
         const channel = client.channels.cache.get(CHANNEL_ID);
-        channel.send(`${targetUser.user.username} is currently in a voice channel. No days missed. Cho would be proud.`);
+        channel.send(`<@${targetUser.user.id}> is currently in a voice channel. No days missed. Cho would be proud.`);
     } else {
         daysMissed++;
         const channel = client.channels.cache.get(CHANNEL_ID);
-        channel.send(`${targetUser.user.username} has not joined a voice channel for ${daysMissed} day(s).`);
+        channel.send(`<@${targetUser.user.id}> hasn't gamed with the homies for ${daysMissed} day(s). :(`);
     }
 }
 
